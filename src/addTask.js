2d9083c5b3ce378107dad6a9fc6fcca2469535cf
Brice Dorsey <s84309@beuth-hochschule.de>
2025-03-09 18:39:48 -0400

    code refractoring

--- src/addTask.js
+++ src/addTask.js
@@ -1,18 +1,11 @@
 "use strict";
 
-const deleteTask = (taskId) => {
-    const tasks = Array.from(document.getElementsByClassName("task"));
-    const taskFound = tasks.find( (task) => task.id == taskId);
-    taskFound.remove();
-}
-
 const addTask = () => {
     const taskNameInput = document.getElementById("task-name");
-    const taskName = taskNameInput.value;
-    console.log(taskName);
+    const taskName = taskNameInput.innerText;
 
     const taskDescriptionInput = document.getElementById("task-description");
-    const taskDescription = taskDescriptionInput.value;
+    const taskDescription = taskDescriptionInput.innerText;
     const taskId = Math.random(10).toString();
 
     const listElement = document.createElement("li");
@@ -24,15 +17,11 @@ const addTask = () => {
     listElement.appendChild(pElement);
 
     const buttonElement = document.createElement("button");
-    buttonElement.innerText = 'Delete';
-    buttonElement.onclick = function() {
-        deleteTask(taskId);
-    }
+    buttonElement.setAttribute("onclick", deleteTask(taskId));
     listElement.appendChild(buttonElement);
 
     const taskElements = document.getElementById("tasks");
     taskElements.appendChild(listElement);
-    console.log(taskElements.getElementById(taskId));
 }
 
 document.getElementById("add-task").addEventListener("click", addTask)
