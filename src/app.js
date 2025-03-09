81141f95978a7aa1cee7c9cf855636a7204079d3
Brice Dorsey <s84309@beuth-hochschule.de>
2025-03-09 16:55:03 -0400

    add template for taks creation

--- src/app.js
+++ src/app.js
@@ -1,2 +1,15 @@
 "use strict";
 
+const deleteTask = (taskId) => {
+    
+    // get <li> element to be deleted
+    // delete <li> element
+    const tasks = new Array.fill(document.getElementsByClassName("task"));
+    tasks.find( (task) => {
+        console.log(task);
+        return true;
+    })
+}
+
+//document.addEventListener("click", deleteTask)
+//document.removeEventListener("click", deleteTask)
