4042f829b179d8d88ee879a11cf7d795094aacb4
Brice Dorsey <s84309@beuth-hochschule.de>
2025-03-09 17:53:26 -0400

    add form and function for adding a new task

--- src/app.js
+++ src/app.js
@@ -1,15 +1,34 @@
 "use strict";
 
 const deleteTask = (taskId) => {
-    
-    // get <li> element to be deleted
-    // delete <li> element
-    const tasks = new Array.fill(document.getElementsByClassName("task"));
-    tasks.find( (task) => {
-        console.log(task);
-        return true;
-    })
+    const tasks = Array.from(document.getElementsByClassName("task"));
+    const taskFound = tasks.find( (task) => task.id == taskId);
+    taskFound.remove();
 }
 
-//document.addEventListener("click", deleteTask)
+const addTask = () => {
+    const taskNameInput = document.getElementById("task-name");
+    const taskName = taskNameInput.innerText;
+
+    const taskDescriptionInput = document.getElementById("task-description");
+    const taskDescription = taskDescriptionInput.innerText;
+    const taskId = Math.random(10).toString();
+
+    const listElement = document.createElement("li");
+    listElement.setAttribute("class", "task");
+    listElement.setAttribute("id", taskId);
+
+    const pElement = document.createElement("p");
+    pElement.innerText = taskName;
+    listElement.appendChild(pElement);
+
+    const buttonElement = document.createElement("button");
+    buttonElement.setAttribute("onclick", deleteTask(taskId));
+    listElement.appendChild(buttonElement);
+
+    const taskElements = document.getElementById("tasks");
+    taskElements.appendChild(listElement);
+}
+
+document.getElementById("add-task").addEventListener("click", addTask)
 //document.removeEventListener("click", deleteTask)
